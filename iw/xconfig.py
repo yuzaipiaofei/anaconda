ff4b2f44aafc1039afddc07bc726ef838b01472e
Matt Wilson <msw@redhat.com>
1999-12-13 20:54:05 +0000

    massive alpha change

--- iw/xconfig.py
+++ iw/xconfig.py
@@ -165,7 +165,11 @@ class XConfigWindow (InstallWindow):
             pass
         else:
             self.didTest = 1
-            
+
+    def memory_cb (self, widget, size):
+        self.todo.x.vidRam = size[:-1]
+        self.todo.x.filterModesByMemory ()
+    
     def getScreen (self):
 	if not self.todo.hdList.packages.has_key('XFree86') or \
 	   not self.todo.hdList.packages['XFree86'].selected: return None
@@ -179,58 +183,86 @@ class XConfigWindow (InstallWindow):
         box = GtkVBox (FALSE, 5)
         box.set_border_width (5)
 
-        self.autoBox = GtkVBox (FALSE, 5)
+        # We can't autoprobe on alpha yet
+        if iutil.getArch() == "alpha":
+            label = GtkLabel (_("You video ram size can not be autodetected.  "
+                                "Choose your video ram size from the choices below:"))
+            label.set_justify (JUSTIFY_LEFT)
+            label.set_line_wrap (TRUE)        
+            label.set_alignment (0.0, 0.5)
+            label.set_usize (400, -1)
+            box.pack_start (label, FALSE)
+            table = GtkTable()
+            group = None
+            count = 0
+            for size in ("256k", "512k", "1024k", "2048k", "4096k",
+                         "8192k", "16384k"):
+                button = GtkRadioButton (group, size)
+                button.connect ('clicked', self.memory_cb, size)
+                if size[:-1] == self.todo.x.vidRam:
+                    button.set_active (1)
+                if not group:
+                    group = button
+                table.attach (button, count % 3, (count % 3) + 1,
+                              count / 3, (count / 3) + 1)
+                count = count + 1
+                
+            box.pack_start (table, FALSE)
+        else:
+            # but we can on everything else
+            self.autoBox = GtkVBox (FALSE, 5)
 
-        label = GtkLabel (_("In most cases your video hardware can "
-                            "be probed to automatically determine the "
-                            "best settings for your display."))
-        label.set_justify (JUSTIFY_LEFT)
-        label.set_line_wrap (TRUE)        
-        label.set_alignment (0.0, 0.5)
-        self.autoBox.pack_start (label, FALSE)
-        
-        label = GtkLabel (_("Autoprobe results:"))
-        label.set_alignment (0.0, 0.5)
-        self.autoBox.pack_start (label, FALSE)
+            label = GtkLabel (_("In most cases your video hardware can "
+                                "be probed to automatically determine the "
+                                "best settings for your display."))
+            label.set_justify (JUSTIFY_LEFT)
+            label.set_line_wrap (TRUE)        
+            label.set_alignment (0.0, 0.5)
+            self.autoBox.pack_start (label, FALSE)
 
-        report = self.todo.x.probeReport ()
-        report = string.replace (report, '\t', '       ')
-        
-        result = GtkLabel (report)
-        result.set_alignment (0.2, 0.5)
-        result.set_justify (JUSTIFY_LEFT)
-        self.autoBox.pack_start (result, FALSE)
+            label = GtkLabel (_("Autoprobe results:"))
+            label.set_alignment (0.0, 0.5)
+            self.autoBox.pack_start (label, FALSE)
+
+            report = self.todo.x.probeReport ()
+            report = string.replace (report, '\t', '       ')
+
+            result = GtkLabel (report)
+            result.set_alignment (0.2, 0.5)
+            result.set_justify (JUSTIFY_LEFT)
+            self.autoBox.pack_start (result, FALSE)
+            box.pack_start (self.autoBox, FALSE)
 
         self.monlist = None
-	if ( not self.sunServer ) and (not self.todo.x.monID or
-                                       self.todo.x.monID == "Generic Monitor"):
+        if (not self.sunServer) and (not self.todo.x.monID or
+                                     self.todo.x.monID == "Generic Monitor"):
             label = GtkLabel (_("Your monitor could not be "
                                 "autodetected. Please choose it "
                                 "from the list below:"))
             label.set_alignment (0.0, 0.5)
             label.set_justify (JUSTIFY_LEFT)
             label.set_line_wrap (TRUE)        
-            self.autoBox.pack_start (label, FALSE)
+            box.pack_start (label, FALSE)
 
             monitors = self.todo.x.monitors ()
             keys = monitors.keys ()
             keys.sort ()
             self.monlist = GtkCList ()
             self.monlist.set_selection_mode (SELECTION_BROWSE)
-	    arch = iutil.getArch()
-	    select = 0
-                    
+            arch = iutil.getArch()
+            select = 0
+
             for monitor in keys:
                 index = self.monlist.append ((monitor,))
                 self.monlist.set_row_data (index, (monitor, monitors[monitor]))
-		if arch == 'sparc' and monitor[:3] == 'Sun':
-		    self.monlist.select_row (index, 0)
-		    select = index
+                if arch == 'sparc' and monitor[:3] == 'Sun':
+                    self.monlist.select_row (index, 0)
+                    select = index
             sw = GtkScrolledWindow ()
             sw.add (self.monlist)
             sw.set_policy (POLICY_NEVER, POLICY_AUTOMATIC)
-            self.autoBox.pack_start (sw, TRUE, TRUE)
-	    self.monlist.moveto (select, 0, 0.5, 0)
+            box.pack_start (sw, TRUE, TRUE)
+            self.monlist.moveto (select, 0, 0.5, 0)
 
 	if not self.sunServer:
 	    test = GtkAlignment ()
@@ -240,18 +272,14 @@ class XConfigWindow (InstallWindow):
         
 	    self.custom = GtkCheckButton (_("Customize X Configuration"))
 	    self.custom.connect ("toggled", self.customToggled)
+	    box.pack_start (test, FALSE)
+	    box.pack_start (self.custom, FALSE)
 
         self.xdm = GtkCheckButton (_("Use Graphical Login"))
-
         self.skip = GtkCheckButton (_("Skip X Configuration"))
         self.skip.connect ("toggled", self.skipToggled) 
 
-	if not self.sunServer:
-	    self.autoBox.pack_start (test, FALSE)
-	    self.autoBox.pack_start (self.custom, FALSE)
-        self.autoBox.pack_start (self.xdm, FALSE)
-
-        box.pack_start (self.autoBox, TRUE, TRUE)
+        box.pack_start (self.xdm, FALSE)
         box.pack_start (self.skip, FALSE)
 
         self.skip.set_active (self.todo.x.skip)
