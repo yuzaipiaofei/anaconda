7b9b543370f5c29d52f1e8f6b8fad9fec140317f
Bill Nottingham <notting@redhat.com>
2001-05-21 17:56:58 +0000

    run efibootmgr in installer %post

--- todo.py
+++ todo.py
@@ -2044,6 +2044,12 @@ class ToDo:
                         pass
                 if unmountUSB:
                     isys.umount(self.instPath + '/proc/bus/usb', removeDir = 0)
+		if arch == 'ia64':
+		    argv = [ "/usr/sbin/efibootmgr", "-c" , "-l",
+		    	     "Red Hat Linux" ]
+		    devnull = os.open("/dev/null", os.O_RDWR)
+		    iutil.execWithRedirect(argv[0], argv, root = self.instPath,
+		    			   stdout = devnull, stdin = devnull, stderr = devnull)
 
 	    w.set(4)
 
