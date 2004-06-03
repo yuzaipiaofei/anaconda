4c005e2979f651810bf5e9a303d1a725bec4ae6e
Paul Nasrat <pnasrat@redhat.com>
2004-06-03 13:29:54 +0000

    First attempt to backup configs

--- migrate.py
+++ migrate.py
@@ -12,10 +12,13 @@
 #
 
 import os
+import os.path
 import md5
 import rpm
+import shutil
 
 from flags import flags
+from product import *
 
 from rhpl.log import log
 from rhpl.translate import _
@@ -28,7 +31,6 @@ def findConfig(intf, id, instPath, dir):
 
     win = intf.waitWindow(_("Finding"), _("Finding existing config files..."))
 
-#TODO: Add progress bar
     for h in mi:
         names=h['filenames']
         fileflags=h['fileflags']
@@ -39,10 +41,49 @@ def findConfig(intf, id, instPath, dir):
             if (fileflags[i] & rpm.RPMFILE_CONFIG) and not (fileflags[i] & rpm.RPMFILE_GHOST) \
                 and isModified(names[i],md5sums[i],instPath):
                     configs.append(names[i])
-#TODO: whitelist/blacklist and do something meaningful
     win.pop()
-    if flags.test:
-        print configs
+    config_full=addWhiteListed(instPath,configs)
+    backupConfig(instPath,config_full)
+
+def backupConfig(instPath,configs):
+    backupDir="%s/var/backup/%s-%s" %( instPath,productPath,productVersion)
+#TODO: exception handling
+    os.makedirs(backupDir)
+    for config in configs:
+        d=os.path.dirname(config)
+        if not os.path.exists(backupDir+d):
+            os.makedirs(backupDir+d)
+        shutil.copy(instPath+config,backupDir+d)
+        
+def addWhiteListed(instPath,configs):
+#TODO: whitelist/blacklist and do something meaningful
+    whitelist = ("/etc/hosts",
+                "/etc/sysconfig/clock",
+                "/etc/sysconfig/desktop",
+                "/etc/sysconfig/grub",
+                "/etc/sysconfig/hwconf",
+                "/etc/sysconfig/i18n",
+                "/etc/sysconfig/installinfo",
+                "/etc/sysconfig/iptables",
+                "/etc/sysconfig/keyboard",
+                "/etc/sysconfig/mouse",
+                "/etc/sysconfig/netdump_id_dsa",
+                "/etc/sysconfig/netdump_id_dsa.pub",
+                "/etc/sysconfig/network",
+                "/etc/sysconfig/selinux",
+                "/etc/ssh/ssh_host_dsa_key.pub",
+                "/etc/ssh/ssh_host_key.pub",
+                "/etc/ssh/ssh_host_dsa_key",
+                "/etc/ssh/ssh_host_key",
+                "/etc/ssh/ssh_host_rsa_key",
+                "/etc/ssh/ssh_host_rsa_key.pub",
+                "/etc/httpd/conf/ssl.key/server.key",
+                "/etc/httpd/conf/ssl.crt/server.crt")
+
+    for file in whitelist:
+        if os.path.exists(instPath + file):
+            configs.append(file)
+    return configs
 
 def isModified(fileName, fileMD5, root="/"):
     m = md5.new()
