24a90479c7c2da3713bbd4120345706929da84e3
Tomas Mlcoch <tmlcoch@redhat.com>
2010-08-23 10:57:19 +0200

    Add tests for pyanaconda/packages.py

---
+++ tests/pyanaconda_test/packages_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import mock
+
+class PackagesTest(mock.TestCase):
+    
+    def setUp(self):
+        self.setupModules(["_isys", "block", "logging"])
+        self.fs = mock.DiskIO()
+        
+        import pyanaconda
+        pyanaconda.anaconda_log = mock.Mock()
+
+        import pyanaconda.packages
+        
+    def tearDown(self):
+        self.tearDownModules()
+    
+    def do_post_action_test(self):
+        import pyanaconda.packages
+        anaconda = mock.Mock()
+        pyanaconda.packages.doPostAction(anaconda)
+        self.assertTrue(anaconda.instClass.postAction.called)
